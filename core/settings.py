55db1c09c0d4a050f3841e17f5266a51a2fd822a
Dilshodbek Donaboyev <d.donaboyev@uic.group>
2025-08-20 01:47:33 +0500

    fix: settings

--- core/settings.py
+++ core/settings.py
@@ -70,6 +70,7 @@ MIDDLEWARE = [
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.TokenAuthentication',
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
