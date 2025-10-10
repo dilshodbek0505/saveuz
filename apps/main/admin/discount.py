cda92ce8d1850c8dbe61f62dc9a0cef131af6f7f
Dilshodbek Donaboyev <d.donaboyev@uic.group>
2025-10-10 22:25:10 +0500

    fix

--- apps/main/admin/discount.py
+++ apps/main/admin/discount.py
@@ -10,7 +10,15 @@ class DiscountAdmin(admin.ModelAdmin):
     
     def formfield_for_foreignkey(self, db_field, request, **kwargs):
         user = request.user
-        if db_field.name == "product" and not user.is_superuser:
+        if db_field.name == "product" and not user.is_superuser and user.is_staff:
             qs = Product.objects.filter(market__owner=user)
             kwargs["queryset"] = qs
         return super().formfield_for_foreignkey(db_field, request, **kwargs)
+    
+    def get_queryset(self, request):
+        user = request.user
+        qs = super().get_queryset(request)
+        qs = qs.select_related("product")
+        if not user.is_superuser and user.is_staff:
+            qs = qs.filter(product__market__owner=user)
+        return qs
